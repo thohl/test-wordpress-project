89794870c687e8805d29abf37fcd05553e25b162
Travis Hohl <thohl@cloud816.com>
2015-02-03 16:19:59 -0600

    Begin configuring Capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'test-wordpress-project'
+set :repo_url, 'git@github.com:thohl/test-wordpress-project.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
